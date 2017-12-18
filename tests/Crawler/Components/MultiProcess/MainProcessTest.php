b411ab5fc630f25e759a485efcbbde1b9fef189e
LL <2504767240@qq.com>
2017-12-18 20:20:01 +0800

    增加多进程单元测试

---
+++ tests/Crawler/Components/MultiProcess/MainProcessTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Crawler\Components\MultiProcess;
+
+use Tests\TestCase;
+
+class MainProcessTest extends TestCase
+{
+    public function testStart()
+    {
+        $process = $this->app->make('MultiProcess', [
+            'handler' => function() {
+                echo posix_getpid();
+            }
+        ]);
+    }
+}
