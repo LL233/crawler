1bb1ccee68b6b670de4dcb60f4e2f553c4d1b4a3
LL <2504767240@qq.com>
2017-12-26 11:36:20 +0800

    修改Downloader接口

--- src/Components/Downloader/DownloaderInterface.php
+++ src/Components/Downloader/DownloaderInterface.php
@@ -14,7 +14,9 @@ interface DownloaderInterface
      * 对一个连接发起请求，并获得连接的内容
      *
      * @param  string $link   请求连接
+     * @param  string $method 请求方式
+     * @param  array  $params 请求的参数
      * @return mixed 请求后获得的内容
      */
-    public function download($link);
+    public function download(string $link, string $method = 'GET', array $params = []);
 }
