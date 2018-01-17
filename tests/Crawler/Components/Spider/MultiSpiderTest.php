21b7fc006206cac10cb605b8aa6cdda4ab617263
LL <2504767240@qq.com>
2018-01-17 11:03:05 +0800

    增加单元测试

--- tests/Crawler/Components/Spider/MultiSpiderTest.php
+++ tests/Crawler/Components/Spider/MultiSpiderTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Crawler\Components\Spider;
 
+use Crawler\EventTag;
 use Tests\TestCase;
+use Crawler\Components\Spider\SpiderEvent;
 
 /**
  * 多进程爬虫单页测试
@@ -11,6 +13,9 @@ use Tests\TestCase;
  */
 class MultiSpiderTest extends TestCase
 {
+    /**
+     * @var \Crawler\Components\Spider\MultiSpider
+     */
     private $multiSpider;
 
     public function __construct()
@@ -25,5 +30,19 @@ class MultiSpiderTest extends TestCase
         $res = $this->multiSpider->getContent('http://www.baidu.com');
 
         $this->assertInstanceOf(\Crawler\Components\Parser\ParserInterface::class, $res);
+    }
+
+    public function testFilterData()
+    {
+        $parser = $this->multiSpider->getContent('http://www.baidu.com');
+
+        $eventDispatcher = $this->container->make('EventDispatcher');
+
+        $eventDispatcher->addListener(EventTag::SPIDER_FILTER_CONTENT_AFTER, function(SpiderEvent $event){
+            $this->assertContainsOnly('array', $event->linkRes);
+            $this->assertContainsOnly('array', $event->dataRes);
+        });
+
+        $this->multiSpider->filterData($parser);
     }
 }
