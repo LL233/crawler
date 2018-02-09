35c9a73ef1d5df1eb798cc5112b37edd5e9f263b
LL <2504767240@qq.com>
2018-02-09 15:43:03 +0800

    修改JsonParser单元测试

--- tests/Crawler/Components/Parser/JsonParserTest.php
+++ tests/Crawler/Components/Parser/JsonParserTest.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: YuJun
- * Date: 2018/2/5
- * Time: 11:35
- */
+
+namespace Tests\Crawler\Components\Parser;
+
+use Tests\TestCase;
+
+class JsonParserTest extends TestCase
+{
+    private $parser;
+
+    public function init()
+    {
+        $this->parser = $this->container->make('JsonParser');
+    }
+
+    public function testParse()
+    {
+        $this->parser->setContent(json_encode(['a', 'b', 'c']));
+
+        $res = $this->parser->parseContent();
+
+        $this->assertInternalType('array', $res);
+        $this->assertCount(3, $res);
+        $this->assertEquals(['a', 'b', 'c'], $res);
+    }
+}
