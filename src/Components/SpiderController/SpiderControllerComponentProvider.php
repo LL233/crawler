793060dd707784d8cbde7bf87252ce9134ab31a0
LL <2504767240@qq.com>
2018-02-23 15:24:58 +0800

    修改爬虫控制器组件的注册方法

--- src/Components/SpiderController/SpiderControllerComponentProvider.php
+++ src/Components/SpiderController/SpiderControllerComponentProvider.php
@@ -14,7 +14,9 @@ class SpiderControllerComponentProvider extends ComponentProvider
     public function register(): void
     {
         $this->container->bind('SpiderController', function($container){
-            return new MultiProcessSpiderController($container->make('Spider'), $container->make('EventDispatcher'));
+            $config = $container->make("Config");
+
+            return new MultiProcessSpiderController($container->make('Spider'), $container->make('EventDispatcher'), $config['sleepTime']);
         });
     }
 }
